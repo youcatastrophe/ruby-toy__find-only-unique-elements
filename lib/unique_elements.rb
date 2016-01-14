e761f9ef9fbcacf60ae5b2c8cc1a135519c735d4
Ryan Newell <ryanalbertnewell@gmail.com>
2016-01-14 20:40:13 +0000

    Finished...I hope

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,5 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    arr.select { |el| arr.count(el) == 1 }
 end
